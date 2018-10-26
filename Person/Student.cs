acc4277200b62e611389e32d29a2e8b4d50107ec
f0s3 <power.aerozoll@gmail.com>
2018-10-26 17:03:29 +0300

    Implement Student methods

--- Person/Student.cs
+++ Person/Student.cs
@@ -1,15 +1,22 @@
-namespace InternshipTest.Person
-{
-    public class Student
-    {
-        public Student(string name)
-        {
-            //TODO: Implementation is needed
+namespace InternshipTest.Person {
+    public class Student {
+        string name;
+        Knowledge knowledge;
+        public Student(string name) {
+            this.name = name;
         }
 
-        public void SetKnowledge(Knowledge knowledge)
-        {
-            //TODO: Implementation is needed
+        public Student(string name, Knowledge knowledge) {
+            this.name = name;
+            SetKnowledge(knowledge);
+        }
+
+        public string GetName() {
+            return this.name;
+        }
+
+        public void SetKnowledge(Knowledge knowledge) {
+            this.knowledge = knowledge;
         }
     }
 }

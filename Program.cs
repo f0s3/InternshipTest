54b6fdf791a9a98ac0f08099f59230308d6b408d
f0s3 <power.aerozoll@gmail.com>
2018-10-26 19:57:54 +0300

    Add some testing data

--- Program.cs
+++ Program.cs
@@ -3,21 +3,21 @@ using InternshipTest.Person;
 using InternshipTest.Institution;
 using InternshipTest.Institution.InterLink;
 
-namespace InternshipTest
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace InternshipTest {
+    class Program {
+        static void Main(string[] args) {
             var s = new Student("Alex");
             University university = new University("CH.U.I.");
             university.AddStudent(new Student("Andrew Kostenko"));
             university.AddStudent(new Student("Julia Veselkina"));
             university.AddStudent(new Student("Maria Perechrest"));
+            university.AddStudent(s);
 
             Internship internship = new Internship("Interlink");
             Console.WriteLine("List of internship's students:");
-            Console.WriteLine(internship.GetStudents());
+            internship.PrintStudentsNames();
+            Console.WriteLine("List of university students:");
+            university.PrintStudentsNames();
         }
     }
 }

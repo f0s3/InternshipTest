0d89d86d1a6f31b6984dc28cd6ee755e9f957781
f0s3 <power.aerozoll@gmail.com>
2018-10-29 15:43:42 +0200

    Add GetAverage and GetStudentsKnowledge methods

--- Institution/University.cs
+++ Institution/University.cs
@@ -1,25 +1,38 @@
 using InternshipTest.Person;
+using InternshipTest.Institution.InterLink;
 using System;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace InternshipTest.Institution {
     public class University {
         private ArrayList studentsList = new ArrayList();
         string name;
         public University(string name) {
-            this.name = name;  
+            this.name = name;
         }
 
         public void AddStudent(Student student) {
             studentsList.Add(student);
         }
 
-        public Student[] GetStudents() {
+        public int GetAverage() {
+            int allLevels = 0;
+            List<int> levelsOfKnowledge = GetStudentsKnowledge();
+            for (int i = 0; i < levelsOfKnowledge.Count; i++) allLevels += levelsOfKnowledge[i];
+            return (int) allLevels/levelsOfKnowledge.Count;//TODO: Math.Round()
+        }
+
+        public Student[] GetStudentsAsArray() {
             return studentsList.ToArray(typeof(Student)) as Student[];
         }
 
+        public ArrayList GetStudents() {
+            return studentsList;
+        }
+
         public string GetStudentsNames() {
-            Student[] studentsArray = GetStudents();
+            Student[] studentsArray = GetStudentsAsArray();
             string s = "[ ";
             if (studentsArray.GetLength(0) == 0) s = "";
             for (int i = 0;i < studentsArray.GetLength(0);i++) {
@@ -33,6 +46,15 @@ namespace InternshipTest.Institution {
                 }
             }
             return s;
+        }
+
+        public List<int> GetStudentsKnowledge() {
+            List<int> studentsLevelOfKnowledge = new List<int>();
+            Student[] studentsArray = GetStudentsAsArray();
+            for (int i = 0;i < studentsArray.GetLength(0);i++) {
+                studentsLevelOfKnowledge.Add(studentsArray[i].GetLevelOfKnowledge());
+            }
+            return studentsLevelOfKnowledge;
         }
     }
 }

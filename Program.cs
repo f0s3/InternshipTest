40c73d11fa5be6558e9ab029b842648a3d040ee6
f0s3 <power.aerozoll@gmail.com>
2018-10-29 15:37:01 +0200

    Add Knowledge levels and use them

--- Program.cs
+++ Program.cs
@@ -6,14 +6,16 @@ using InternshipTest.Institution.InterLink;
 namespace InternshipTest {
     class Program {
         static void Main(string[] args) {
-            var s = new Student("Alex");
+            var alex = new Student("Alex");
+            alex.SetKnowledge(new Knowledge(5));
             University university = new University("CH.U.I.");
-            university.AddStudent(new Student("Andrew Kostenko"));
-            university.AddStudent(new Student("Julia Veselkina"));
+            university.AddStudent(new Student("Andrew Kostenko", new Knowledge(4)));
+            university.AddStudent(new Student("Julia Veselkina", new Knowledge(7)));
             university.AddStudent(new Student("Maria Perechrest"));
-            university.AddStudent(s);
+            university.AddStudent(alex);
 
             Internship internship = new Internship("Interlink");
+            internship.AddStudentsFromUniversity(university);
             Console.WriteLine("List of internship's students:");
             Console.WriteLine(internship.GetStudentsNames());
             Console.WriteLine("List of university students:");
